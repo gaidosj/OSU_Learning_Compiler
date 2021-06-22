5cbb0a2228e82f2c64570d051d8bf9cdba9f0c56
gaidosj <56010019+gaidosj@users.noreply.github.com>
2021-06-21 22:04:36 -0400

    test pull with updated test

--- tests.py
+++ tests.py
@@ -11,6 +11,10 @@ class TestCase(unittest.TestCase):
     def test2(self):
         out = "Goodbye"
         self.assertNotEqual(hello.hello(), out)
+        
+    def test3(self):
+        out = "testpull"
+        self.assertNotEqual(hello.hello(), out)
 
 
 if __name__ == '__main__':
