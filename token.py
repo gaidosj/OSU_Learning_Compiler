f1372eeba72cf1d31f91a1794e6d3cf314b5f0bb
gaidosj <56010019+gaidosj@users.noreply.github.com>
2021-07-01 20:38:37 -0700

    created token file, Lexer class; building lexer

---
+++ token.py
@@ -0,0 +1,41 @@
+# This contains all the language tokens for use by the lexer
+
+# Types
+INT = 'int'
+BOOL = 'bool'
+STRING = 'string'
+VAR = 'var'
+
+# Math operators
+EQUALS = '='
+PLUS = '+'
+MINUS = '-'
+ASTERISK = '*'
+DIV = '/'
+
+# Logic operators
+LT = '<'
+GT = '>'
+EQUALITY = '=='
+INEQUALITY = '!='
+
+# Bracket types
+LEFT_PAREN = '('
+RIGHT_PAREN = ')'
+LEFT_CURLY = '{'
+RIGHT_CURLY = '}'
+
+# Keywords
+FUNCTION = 'func'
+RETURN = 'return'
+IF = 'if'
+ELSE = 'else'
+ELIF = 'elif'
+WHILE = 'while'
+COMMA = ','
+SEMICOLON = ';'
+PRINT = 'print'
+
+# Macro objects
+EOF = 'EOF'
+INCLUDE = 'include'
