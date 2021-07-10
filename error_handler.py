39412c6e992cbb0ac0d9ea601b634110ba344ab5
Tony Fiore <tdfiore@gmail.com>
2021-07-09 20:42:21 -0700

    error_handler.py

---
+++ error_handler.py
@@ -0,0 +1,14 @@
+from sys import stderr
+
+class ErrorHandler:
+    def __init__(self):
+        self.output = stderr
+
+    def report_error(self, module, line_num, message):
+        self.output.write(
+            module
+            + " error on line "
+            + line_num
+            + " : "
+            + message
+        )
