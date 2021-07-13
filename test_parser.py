edb9ff2da2b56185ea056e12b17e4d5e2d6fb013
Tony Fiore <tdfiore@gmail.com>
2021-07-12 20:28:00 -0700

    Add more parser tests

--- test_parser.py
+++ test_parser.py
@@ -13,9 +13,16 @@ class ParserTest(unittest.TestCase):
                 TokenType.IDENTIFIER, 'name', 'identifier', 1),
             TokenType.INT: Token(TokenType.INT, '1', 1, 1),
             TokenType.MINUS: Token(TokenType.MINUS, '-', '-', 1),
+            TokenType.NOT: Token(TokenType.NOT, '!', '!', 1),
             TokenType.ASTERISK: Token(TokenType.ASTERISK, '*', '*', 1),
             TokenType.DIV: Token(TokenType.DIV, '/', '/', 1),
-            TokenType.PLUS: Token(TokenType.PLUS, '+', '+', 1)
+            TokenType.PLUS: Token(TokenType.PLUS, '+', '+', 1),
+            TokenType.GTE: Token(TokenType.GTE, '>=', '>=', 1),
+            TokenType.GT: Token(TokenType.GT, '>', '>', 1),
+            TokenType.LTE: Token(TokenType.LTE, '<=', '<=', 1),
+            TokenType.LT: Token(TokenType.LT, '<', '<', 1),
+            TokenType.EQUALITY: Token(TokenType.EQUALITY, '==', '==', 1),
+            TokenType.INEQUALITY: Token(TokenType.INEQUALITY, '!=', '!=', 1)
         }
 
     def test_empty_input(self):
@@ -37,7 +44,7 @@ class ParserTest(unittest.TestCase):
             expression.Literal(self.tokens[TokenType.IDENTIFIER])
         )
 
-    def test_simple_unary(self):
+    def test_unary_minus(self):
         self.parser.tokens = [
             self.tokens[TokenType.MINUS],
             self.tokens[TokenType.INT]
@@ -51,7 +58,21 @@ class ParserTest(unittest.TestCase):
             )
         )
 
-    def test_simple_factor(self):
+    def test_unary_not(self):
+        self.parser.tokens = [
+            self.tokens[TokenType.NOT],
+            self.tokens[TokenType.INT]
+        ]
+
+        self.assertEqual(
+            self.parser.parse(),
+            expression.Unary(
+                self.tokens[TokenType.NOT],
+                expression.Literal(self.tokens[TokenType.INT])
+            )
+        )
+
+    def test_asterisk(self):
         self.parser.tokens = [
             self.tokens[TokenType.IDENTIFIER],
             self.tokens[TokenType.ASTERISK],
@@ -67,7 +88,23 @@ class ParserTest(unittest.TestCase):
             )
         )
 
-    def test_unary_inside_factor(self):
+    def test_slash(self):
+        self.parser.tokens = [
+            self.tokens[TokenType.INT],
+            self.tokens[TokenType.DIV],
+            self.tokens[TokenType.IDENTIFIER]
+        ]
+
+        self.assertEqual(
+            self.parser.parse(),
+            expression.Binary(
+                expression.Literal(self.tokens[TokenType.INT]),
+                self.tokens[TokenType.DIV],
+                expression.Literal(self.tokens[TokenType.IDENTIFIER])
+            )
+        )
+
+    def test_initial_minus(self):
         self.parser.tokens = [
             self.tokens[TokenType.MINUS],
             self.tokens[TokenType.INT],
@@ -87,7 +124,7 @@ class ParserTest(unittest.TestCase):
             )
         )
 
-    def test_simple_term(self):
+    def test_plus(self):
         self.parser.tokens = [
             self.tokens[TokenType.IDENTIFIER],
             self.tokens[TokenType.PLUS],
@@ -100,6 +137,46 @@ class ParserTest(unittest.TestCase):
                 expression.Literal(self.tokens[TokenType.IDENTIFIER]),
                 self.tokens[TokenType.PLUS],
                 expression.Literal(self.tokens[TokenType.IDENTIFIER])
+            )
+        )
+
+    def test_binary_minus(self):
+        self.parser.tokens = [
+            self.tokens[TokenType.INT],
+            self.tokens[TokenType.MINUS],
+            self.tokens[TokenType.INT]
+        ]
+
+        self.assertEqual(
+            self.parser.parse(),
+            expression.Binary(
+                expression.Literal(self.tokens[TokenType.INT]),
+                self.tokens[TokenType.MINUS],
+                expression.Literal(self.tokens[TokenType.INT])
+            )
+        )
+
+    def test_unary_binary_minuses(self):
+        self.parser.tokens = [
+            self.tokens[TokenType.MINUS],
+            self.tokens[TokenType.IDENTIFIER],
+            self.tokens[TokenType.MINUS],
+            self.tokens[TokenType.MINUS],
+            self.tokens[TokenType.IDENTIFIER]
+        ]
+
+        self.assertEqual(
+            self.parser.parse(),
+            expression.Binary(
+                expression.Unary(
+                    self.tokens[TokenType.MINUS],
+                    expression.Literal(self.tokens[TokenType.IDENTIFIER])
+                ),
+                self.tokens[TokenType.MINUS],
+                expression.Unary(
+                    self.tokens[TokenType.MINUS],
+                    expression.Literal(self.tokens[TokenType.IDENTIFIER])
+                )
             )
         )
 
@@ -126,6 +203,124 @@ class ParserTest(unittest.TestCase):
                     self.tokens[TokenType.ASTERISK],
                     expression.Literal(self.tokens[TokenType.INT])
                 )
+            )
+        )
+
+    def test_gte(self):
+        self.parser.tokens = [
+            self.tokens[TokenType.IDENTIFIER],
+            self.tokens[TokenType.GTE],
+            self.tokens[TokenType.INT]
+        ]
+
+        self.assertEqual(
+            self.parser.parse(),
+            expression.Binary(
+                expression.Literal(self.tokens[TokenType.IDENTIFIER]),
+                self.tokens[TokenType.GTE],
+                expression.Literal(self.tokens[TokenType.INT])
+            )
+        )
+
+    def test_gt(self):
+        self.parser.tokens = [
+            self.tokens[TokenType.IDENTIFIER],
+            self.tokens[TokenType.GT],
+            self.tokens[TokenType.INT]
+        ]
+
+        self.assertEqual(
+            self.parser.parse(),
+            expression.Binary(
+                expression.Literal(self.tokens[TokenType.IDENTIFIER]),
+                self.tokens[TokenType.GT],
+                expression.Literal(self.tokens[TokenType.INT])
+            )
+        )
+
+    def test_lte(self):
+        self.parser.tokens = [
+            self.tokens[TokenType.IDENTIFIER],
+            self.tokens[TokenType.LTE],
+            self.tokens[TokenType.INT]
+        ]
+
+        self.assertEqual(
+            self.parser.parse(),
+            expression.Binary(
+                expression.Literal(self.tokens[TokenType.IDENTIFIER]),
+                self.tokens[TokenType.LTE],
+                expression.Literal(self.tokens[TokenType.INT])
+            )
+        )
+
+    def test_lt(self):
+        self.parser.tokens = [
+            self.tokens[TokenType.IDENTIFIER],
+            self.tokens[TokenType.LT],
+            self.tokens[TokenType.INT]
+        ]
+
+        self.assertEqual(
+            self.parser.parse(),
+            expression.Binary(
+                expression.Literal(self.tokens[TokenType.IDENTIFIER]),
+                self.tokens[TokenType.LT],
+                expression.Literal(self.tokens[TokenType.INT])
+            )
+        )
+
+    def test_term_then_comparison(self):
+        self.parser.tokens = [
+            self.tokens[TokenType.IDENTIFIER],
+            self.tokens[TokenType.PLUS],
+            self.tokens[TokenType.INT],
+            self.tokens[TokenType.GT],
+            self.tokens[TokenType.INT]
+        ]
+
+        self.assertEqual(
+            self.parser.parse(),
+            expression.Binary(
+                expression.Binary(
+                    expression.Literal(self.tokens[TokenType.IDENTIFIER]),
+                    self.tokens[TokenType.PLUS],
+                    expression.Literal(self.tokens[TokenType.INT])
+                ),
+                self.tokens[TokenType.GT],
+                expression.Literal(self.tokens[TokenType.INT])
+            )
+        )
+
+    def test_equality(self):
+        self.parser.tokens = [
+            self.tokens[TokenType.IDENTIFIER],
+            self.tokens[TokenType.EQUALITY],
+            self.tokens[TokenType.INT]
+        ]
+
+        self.assertEqual(
+            self.parser.parse(),
+            expression.Binary(
+                expression.Literal(self.tokens[TokenType.IDENTIFIER]),
+                self.tokens[TokenType.EQUALITY],
+                expression.Literal(self.tokens[TokenType.INT])
+            )
+        )
+
+    def test_inequality(self):
+        self.parser.tokens = [
+            self.tokens[TokenType.INT],
+            self.tokens[TokenType.INEQUALITY],
+            self.tokens[TokenType.IDENTIFIER]
+        ]
+
+        self.assertEqual(
+            self.parser.parse(),
+            expression.Binary(
+                expression.Literal(self.tokens[TokenType.INT]),
+                self.tokens[TokenType.INEQUALITY],
+                expression.Literal(self.tokens[TokenType.IDENTIFIER])
             )
         )
 
