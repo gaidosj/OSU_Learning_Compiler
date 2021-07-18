f10e92795843ea3fd89b608b5761d8eaae385a00
Tony Fiore <tdfiore@gmail.com>
2021-07-18 16:58:59 -0700

    Protect against adding duplicate entries to pythonpath

--- tests/parent_dir.py
+++ tests/parent_dir.py
@@ -1,2 +1,4 @@
-from os import sys, path
-sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+from os import sys, path, getenv, environ
+if (getenv('OLC_PROJ_DIR') is None):
+    environ['OLC_PROJ_DIR'] = path.dirname(path.dirname(path.abspath(__file__)))
+    sys.path.append(getenv('OLC_PROJ_DIR'))
