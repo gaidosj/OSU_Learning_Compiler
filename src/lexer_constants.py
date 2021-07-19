103a45009d02c38aef236adf6176f0c929a7293c
gbarbachov <barbchov@oregonstate.edu>
2021-07-18 23:16:56 -0700

    small lrefactor of lexer code for readability

---
+++ src/lexer_constants.py
@@ -0,0 +1,55 @@
+from string import ascii_lowercase, digits
+from tokens import TokenType
+
+RESERVED_WORDS = {
+    'var': TokenType.VAR,
+    'function': TokenType.FUNCTION,
+    'return': TokenType.RETURN,
+    'if': TokenType.IF,
+    'else': TokenType.ELSE,
+    'elif': TokenType.ELIF,
+    'while': TokenType.WHILE,
+    'print': TokenType.PRINT,
+    'include': TokenType.INCLUDE,
+    'class': TokenType.CLASS,
+}
+
+SINGLE_TOKENS = {
+    '(': TokenType.LEFT_PAREN,
+    ')': TokenType.RIGHT_PAREN,
+    '{': TokenType.LEFT_CURLY,
+    '}': TokenType.RIGHT_CURLY,
+    '[': TokenType.LEFT_SQUARE,
+    ']': TokenType.RIGHT_SQUARE,
+    ',': TokenType.COMMA,
+    '.': TokenType.DOT,
+    '-': TokenType.MINUS,
+    '+': TokenType.PLUS,
+    ';': TokenType.SEMICOLON,
+    '*': TokenType.ASTERISK,
+    '^': TokenType.NOT,
+    '\n': TokenType.EOL,
+}
+
+DOUBLE_TOKENS = {
+    '=': {'match': '=', 'yes': TokenType.EQUALITY, 'no': TokenType.EQUALS},
+    '!': {'match': '=', 'yes': TokenType.INEQUALITY, 'no': TokenType.NOT},
+    '>': {'match': '=', 'yes': TokenType.GTE, 'no': TokenType.GT},
+    '<': {'match': '=', 'yes': TokenType.LTE, 'no': TokenType.LT},
+    '&': {'match': '&', 'yes': TokenType.AND, 'no': TokenType.ERROR},
+    '|': {'match': '|', 'yes': TokenType.OR, 'no': TokenType.ERROR},
+}
+
+DISREGARDED_WHITESPACES = {
+    ' ', '\r', '\t'
+}
+
+STRING_LITERALS = {
+    '"', "'"
+}
+
+NUMBER_LITERALS = set(digits)
+
+IDENTIFIER_LITERALS = set(ascii_lowercase).union({'_'})
+
+END_OF_LINE = '\n'
