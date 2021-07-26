107e24ade232a3094668705fbd46bc6496f4490b
gbarbachov <barbchov@oregonstate.edu>
2021-07-26 03:22:29 -0700

    linter

--- src/interpreter_operators.py
+++ src/interpreter_operators.py
@@ -1,5 +1,3 @@
-from src.tokens import TokenType, TokenOsu
-from src.interpreter_runtime import RuntimeDataType, RuntimeValue
-from src.error_handler import InterpretError
-
-
+# from src.tokens import TokenType, TokenOsu
+# from src.interpreter_runtime import RuntimeDataType, RuntimeValue
+# from src.error_handler import InterpretError
