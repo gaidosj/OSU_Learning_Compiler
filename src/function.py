8e62a0422cd5d5ab6a778ca53500b4874074bafc
Tony Fiore <tdfiore@gmail.com>
2021-07-28 23:50:51 -0700

    Add arity

--- src/function.py
+++ src/function.py
@@ -5,9 +5,15 @@ class Function:
     def __init__(self, declaration):
         self.declaration = declaration
 
+    def arity(self):
+        return len(self.declaration.parameters)
+
     def call(self, interpreter, arguments):
         environment = Environment(interpreter.environment)
         for i in range(len(self.declaration.parameters)):
             environment.define(self.declaration.parameters[i].lexeme, arguments[i])
 
         interpreter.execute_block(self.declaration.body, environment)
+
+    def __str__(self):
+        return '<function ' + self.declaration.name.lexeme + '>'
